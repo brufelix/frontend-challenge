8e4e191c6700ca589c02685c2a5f2a44b9551cee
Bruno Dias <bruno.dias@enacom.com.br>
2024-09-01 10:59:44 -0300

    feat: setting environment variables via docker compose and adding new comments

--- web/src/hooks/use-basket.hook.ts
+++ web/src/hooks/use-basket.hook.ts
@@ -1,4 +1,5 @@
 import { useContext } from "react";
 import { BasketContext } from "../providers/Basket.provider";
 
+// Hook para os itens selecionado no carrinho
 export const useBasket = () => useContext(BasketContext);
